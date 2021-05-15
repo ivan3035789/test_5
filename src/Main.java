ee0fe81009c0353847fd413a4ba03073448ae949
ivan3035789 <82367614+ivan3035789@users.noreply.github.com>
2021-05-15 12:41:37 +0300

    update main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int initialAccount = 300;
-        int topUp = 1500;
+        int topUp = 150;
         int forEvery100Rubles = 100;
         int bonus = topUp / forEvery100Rubles;
+        int totalSum = initialAccount + topUp;
         if (topUp > 1000) {
-            initialAccount += bonus;
+            initialAccount = bonus + totalSum;
         } else {
             bonus = 0;
+            initialAccount += topUp;
         }
+
         System.out.println("Зачисленно бонусных рублей:" + bonus + "\nБаланс:" + initialAccount + " рублей");
     }
 }
